c58adf737fef5408cde20716265ab16cfb8c4823
Tim <timkentdev@gmail.com>
2026-03-26 15:37:15 -0700

    Chore: refine person-film schema to enforce cast position (#7)

--- src/PersonFilm/PersonFilm.schema.ts
+++ src/PersonFilm/PersonFilm.schema.ts
@@ -1,27 +1,40 @@
 import * as z from "zod";
 
-const PersonFilmSchema = z.object({
-	filmSlug: z.string().min(1).max(64),
-	personSlug: z.string().min(1).max(64),
-	role: z.preprocess(
-		(val) => {
-			if (typeof val === "string") {
-				return val.toLowerCase();
+const PersonFilmSchema = z
+	.object({
+		filmSlug: z.string().min(1).max(64),
+		personSlug: z.string().min(1).max(64),
+		role: z.preprocess(
+			(val) => {
+				if (typeof val === "string") {
+					return val.toLowerCase();
+				}
+				return val;
+			},
+			z.enum(["actor", "director", "writer"]),
+		),
+		castPosition: z.preprocess(
+			(val) => {
+				if (typeof val === "string") {
+					if (!val.length) return null;
+					return Number(val);
+				}
+				return val;
+			},
+			z.union([z.int().min(1).max(5), z.null()]),
+		),
+	})
+	.superRefine((data, ctx) => {
+		if (data.role === "actor") {
+			if (!data.castPosition) {
+				ctx.addIssue({
+					message: "Actor role must have a cast position",
+					code: "custom",
+				});
 			}
-			return val;
-		},
-		z.enum(["actor", "director", "writer"]),
-	),
-	castPosition: z.preprocess(
-		(val) => {
-			if (typeof val === "string") {
-				if (!val.length) return null;
-				return Number(val);
-			}
-			return val;
-		},
-		z.union([z.int().min(1).max(5), z.null()]),
-	),
-});
+		} else {
+			ctx.value.castPosition = null;
+		}
+	});
 
 export default PersonFilmSchema;
